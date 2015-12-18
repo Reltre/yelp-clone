f214d0be2339ac15a00ca103e42d40ab3ade1534
Reltre <tglippert@gmail.com>
2015-12-17 17:02:48 -0800

    Test create action in reviews controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,4 +3,18 @@ class ReviewsController < ApplicationController
   def new
     @review = Review.new
   end
+
+  def create
+
+    review = Review.new(review_params)
+    review.business_id = params[:business_id]
+    review.save
+    redirect_to business_path(params[:business_id])
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:rating, :content)
+  end
 end
--- spec/controllers/reviews_controller_spec.rb
+++ spec/controllers/reviews_controller_spec.rb
@@ -1,10 +1,50 @@
 require 'rails_helper'
 
 describe ReviewsController do
+
+  let(:business_id) { Fabricate(:business).id }
+
   describe "GET new" do
-    let(:business_id) { Fabricate(:business).id }
     before { get :new, business_id: business_id }
 
     it { expect(assigns(:review)).to be_a_new(Review) }
+  end
+
+  describe "POST create" do
+    #expects data of the type review { "rating" => "some number", "content" => "string"}
+    context "when authenticated" do
+
+      context "with valid input" do
+        it "saves a review" do
+          post :create, business_id: business_id,
+                        review: { "rating": "3", "content": "Meh." }
+          expect(Review.count).to eq(1)
+        end
+
+        it "associates the correct business with the review" do
+          post :create, business_id: business_id,
+                        review: { "rating": "3", "content": "Meh." }
+          expect(Review.first.business.id).to eq(business_id)
+        end
+
+        it "associates the current user with a new review"
+        it "sets a flash success message"
+      end
+
+      context "with invalid input" do
+        it "sets a flash error message"
+      end
+    end
+
+    context "when not authenticated" do
+      it "redirects to the home page"
+      it "displays an error message"
+    end
+  end
+
+  it "redirects to the business page" do
+    post :create, business_id: business_id,
+                  review: { "rating": "3", "content": "Meh." }
+    expect(response).to redirect_to(business_path(business_id))
   end
 end
