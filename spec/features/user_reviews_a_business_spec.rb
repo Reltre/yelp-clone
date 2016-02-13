db49bbb0528905cca5f5058725c997260ce1c6d7
Reltre <tglippert@gmail.com>
2016-02-13 11:04:13 -0800

    test the review feature for a business

--- spec/features/user_reviews_a_business_spec.rb
+++ spec/features/user_reviews_a_business_spec.rb
@@ -11,5 +11,12 @@ feature "User navigates to a business" do
       find('.business-row a:first-child').click
     end
     expect(current_path).to eq(business_path(business.id))
+    click_link("+ New Review")
+    fill_in("review_rating", with: "3")
+    fill_in("review_content", with: "test12")
+    click_button("Post Review")
+    within("#business-show-reviews") do
+      expect(page).to have_text "test12"
+    end
   end
 end
