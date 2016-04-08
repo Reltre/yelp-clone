42b72575f17c7e84cffca5c08117c0d1fd669e64
Reltre <tglippert@gmail.com>
2016-04-08 11:44:33 -0700

    feature test for going to another users's page.

---
+++ spec/features/navigate_to_other_user_page_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+feature "Navigate to another user's page" do
+  given!(:user) { Fabricate(:user, first_name: "Jane", last_name: "Robbins") }
+  given!(:business) { Fabricate(:business) }
+  given!(:review) { Fabricate(:review, user: user, business: business) }
+
+  scenario "through a posted review" do
+    visit home_path
+    within('div#recent-reviews') do
+      find('.review .col-md-5 h5 a', text: 'Jane Robbins').click
+    end
+    expect(current_path).to eq(user_path(user.id))
+  end
+end
